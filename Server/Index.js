0da4d3a929d3a53874dca86b9918cd58cd5a32b2
CatalinOSBI <garjoaba_c@yahoo.com>
2024-07-14 18:14:10 +0300

    added timer, better system (create objects for tokens)

--- Server/Index.js
+++ Server/Index.js
@@ -1,40 +1,56 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 3000
+const bodyParser = require('body-parser')
+const port = 8080
 
 //CORS
 app.use(cors())
+//Body parser
+app.use(bodyParser.json())
+
+let userData = {}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-function handleTest() {
-  return ('testfunc')
-}
-
 let token = null
-let comicObject = null
+let foldersObject = null
 
-app.get('/GetData/:token/:comicObject', (req, res) => {
+app.post('/GetData/:token', (req, res) => {
 
   token = req.params.token
-  comicObject = req.params.comicObject
+  foldersObject = req.body
+
+  //Add Data property and store the folderObject inside
+  userData[token] = {
+    data: foldersObject,
+  }
 
   res.send('Data received')
+  console.log('(EXPORT) Data received:')
+  console.log(`Token: ${token}`)
+  console.log(foldersObject)
+
+  //Timer to delete the token after 5 minutes
+  setTimeout(() => {
+    delete userData[token]
+    console.log(`Token ${token} has been removed from the pool`)
+  }, 60000);
 })
 
 app.get(`/Token/:dynamicToken`, (req, res) => {
   const dynamicToken = req.params.dynamicToken
 
-  if (dynamicToken === token) {
-    res.send('Show Data')
-    console.log(dynamicToken)
-    console.log(comicObject)
-    //Reset
-    token = null
-    comicObject = null
+  if (userData.hasOwnProperty(dynamicToken)) {
+    //Update user data (the folders)
+    const userFolders = userData[dynamicToken].data
+    res.json(userFolders)
+    console.log('(IMPORT) Data received:')
+    console.log(`Token: ${token}`)
+    console.log(userFolders)
+
   } else {
     res.status(404).send('Token not found')
   } 
@@ -42,9 +58,8 @@ app.get(`/Token/:dynamicToken`, (req, res) => {
 })
 
 app.get('/test', (req, res) => {
-  res.send(token)
+  res.json(Object.keys(userData))
 })
-
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
